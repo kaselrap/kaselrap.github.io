acfdadc8f83d05618db0e4c1d5c9e7427a0643c3
stanislav <kaselrao@gmail.com>
2020-04-22 21:16:49 +0300

    Added statistics

---
+++ statistics/sort.js
@@ -0,0 +1,31 @@
+
+function sorting() {
+  var lines = document.getElementById('sort').value.split('\n');
+  lines = lines.filter(function(el) { return el; });
+  var list = [];
+  for (key in lines) {
+    var object = lines[key].split(',');
+    list.push({
+      name: String(object[0].replace(/"/gi, '')),
+      value: parseInt(object[1].replace(/"/gi, ''))
+    });
+  }
+  var groupedListByValue = Object.assign([], groupBy('value', list)).reverse();
+
+  var i = 1;
+  for (key in groupedListByValue) {
+      for (newKey in groupedListByValue[key]) {
+        $('.result').append('<p>' + (newKey == 0 ? i + '. ' : '') + groupedListByValue[key][newKey].name + ' ' + groupedListByValue[key][newKey].value + '</p>');
+      }
+      i++;
+  }
+  
+}
+
+function groupBy (key, array) {
+    return array.reduce((objectsByKeyValue, obj) => {
+        const value = obj[key];
+        objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+        return objectsByKeyValue;
+    }, {});
+}
